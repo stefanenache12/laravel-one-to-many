8dc8f18734aec8a2ac53cbf068440274ef238229
stefanenache12 <stefanenache12@gmail.com>
2023-09-21 16:29:18 +0200

    ADD TYPE MODEL/MIGRATION

--- app/Models/Type.php
+++ app/Models/Type.php
@@ -5,12 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Project extends Model
+class Type extends Model
 {
     use HasFactory;
 
-    public function projectType()
+    public function project()
     {
-        return $this->belongsTo(Type::class);
+        return $this->hasMany(Project::class);
     }
 }
